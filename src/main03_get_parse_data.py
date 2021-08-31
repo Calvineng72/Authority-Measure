9596e2e0f0f7cc26c5aa0bff278252ec5bd52a97
dominiksinsaarland <dominik.stammbach@bluewin.ch>
2021-08-31 17:07:24 +0200

    added code

---
+++ src/main03_get_parse_data.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+
+from collections import Counter
+import os
+import argparse
+import pandas as pd
+import joblib
+from tqdm import tqdm
+
+                        
+other = ['agreement',
+         'day',
+         'assignment','leave','payment',
+         'he/she','they','party']
+
+conditionals = ['if','when','unless']
+	#'where', 'whereas','whenever', 'provided that', 'in case']
+    
+    
+def extract_pdata(args):
+    subcount = Counter()
+    subnouncount = Counter()
+    modalcount = Counter()
+
+    num_to_process = len(os.listdir(args.input_directory))
+
+    iteration_num = 0
+    chunk_num = 0
+    pdata_rows = []
+
+    files = os.listdir(os.path.join(args.output_directory, "02_parsed_articles"))
+    filenames = [os.path.join(args.output_directory, "02_parsed_articles", fn) for fn in files]
+    for filename in tqdm(filenames, total=len(filenames)):
+        for statement_data in joblib.load(filename):
+            contract_id = statement_data["contract_id"]
+            # skip french contracts
+            if not "eng" in contract_id:
+                continue
+            # Loop over each statement, getting the subject/subject_branch/subject_tag
+            subject = statement_data['subject']
+            statement_dict = {'contract_id':contract_id,
+            'article_num':statement_data['article_num'],
+            'sentence_num':statement_data['sentence_num'],
+            'statement_num':statement_data['statement_num'],
+            'full_sentence':statement_data['full_sentence'],
+            'full_statement':statement_data['full_statement'],
+            'subject':statement_data['subject'], 'passive':statement_data['passive'],
+            'subject_tags':statement_data['subject_tags'],
+            'subject_branch':statement_data['subject_branch'],    
+            'object_tags':statement_data['object_tags'],
+            'verb':statement_data['verb'], 'modal':statement_data['modal'],
+            'md':statement_data['md'], 'neg':statement_data['neg'],
+            'object_branches':statement_data['object_branches']}
+
+            pdata_rows.append(statement_dict)
+            subjectnouns = sorted([x for x,t in zip(statement_data['subject_branch'], statement_data['subject_tags']) if t.startswith('N')])
+            subcount[subject] += 1
+            if statement_data['md'] == 1:
+                modalcount[subject] += 1
+            for x in subjectnouns:
+                if x != subject:
+                    subnouncount[x] += 1
+            iteration_num = iteration_num + 1
+            # Print a message and save the statements every 100k
+            if iteration_num % 100000 == 0:
+                print("Iteration ", iteration_num)
+                cur_df = pd.DataFrame(pdata_rows)
+                cur_df.to_pickle(os.path.join(args.output_directory, "03_pdata", "pdata_" + str(chunk_num) + ".pkl"))
+                chunk_num += 1
+                pdata_rows.clear()
+
+    # Make a Pandas df out of whatever's left in pdata_rows and save it
+    cur_df = pd.DataFrame(pdata_rows)
+    cur_df.to_pickle(os.path.join(args.output_directory, "03_pdata", "pdata_" + str(chunk_num) + ".pkl"))
+    sub_counts_filename = os.path.join(args.output_directory, "subject_counts.pkl")
+    joblib.dump(subcount, sub_counts_filename)
+    modal_counts_filename = os.path.join(args.output_directory, "modal_counts.pkl")
+    joblib.dump(subcount, sub_counts_filename)    
+    print("most common subjects", subcount.most_common()[:100])
+    
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_directory", type=str, default="")
+    parser.add_argument("--output_directory", type=str, default="")
+    args = parser.parse_args()
+
+    try:
+        os.mkdir(os.path.join(args.output_directory, "03_pdata"))
+    except:
+        pass
+    extract_pdata(args)
+
---
+++ src/pipeline.py
@@ -0,0 +1,62 @@
+import argparse
+import json
+import os
+from tqdm import tqdm
+# 3rd party imports
+import spacy
+import neuralcoref
+import sys
+from collections import defaultdict
+from main02_parse_articles import parse_article
+from main03_get_parse_data import extract_pdata
+from main04_compute_auth import combine_auth, compute_statement_auth
+
+
+class Pipeline():
+	def __init__(self, args):
+		self.args = args
+		os.makedirs(self.args.output_directory, exist_ok=True)
+		self.nlp = spacy.load('en_core_web_sm', disable=["ner"])
+		neuralcoref.add_to_pipe(self.nlp)
+
+	def parse_articles(self):
+		for filename in tqdm(os.listdir(args.input_directory)):
+			parse_article(filename, self.nlp, self.args)
+
+	def extract_parsed_data(self):
+		extract_pdata(self.args)
+
+	def compute_authority_measures(self):
+		chunks = os.listdir(os.path.join(args.output_directory, "03_pdata"))
+		chunks = sorted(chunks, key=lambda x: int(x.split("_")[-1][:-4]))
+		for filename in tqdm(chunks):
+			filepath = os.path.join(args.output_directory, "03_pdata", filename)
+			cur_df = pd.read_pickle(filepath)
+			compute_statement_auth(args, cur_df, filename)
+		combine_auth
+
+	def run_main(self):
+		# dependency parsing
+		os.makedirs(os.path.join(self.args.output_directory, "02_parsed_articles"), exist_ok=True)
+		self.parse_articles()
+
+		# extract necessary parsed information
+		os.makedirs(os.path.join(self.args.output_directory, "03_pdata"), exist_ok=True)
+		self.extract_parsed_data()
+
+		os.makedirs(os.path.join(self.args.output_directory, "04_auth"), exist_ok=True)
+
+		# compute authority measures for chunks
+		self.compute_authority_measures()
+
+
+		
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--input_directory", type=str, default="")
+	parser.add_argument("--output_directory", type=str, default="")
+	args = parser.parse_args()
+	pipeline = Pipeline(args)
+	pipeline.run_main()
+
+
